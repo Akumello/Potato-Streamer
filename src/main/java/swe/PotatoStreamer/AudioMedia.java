cf93919eeed3841b8bfbdb0fa12d5cd9250daedf
Michael Gallahan <mgallaha@gmu.edu>
2019-11-26 14:02:37 -0500

    Done

---
+++ src/main/java/swe/PotatoStreamer/AudioMedia.java
@@ -0,0 +1,61 @@
+package swe.PotatoStreamer;
+
+public class AudioMedia {
+	/**
+	 * shell class to contain song information, should make things easier for
+	 * upload/download
+	 */
+	private String username;
+	private String song_name;
+	private String song_artist;
+	private String song_album;
+	private String path;
+
+	public AudioMedia(String user, String name, String artist, String album) {
+		setUsername(user);
+		song_name = name;
+		song_artist = artist;
+		song_album = album;
+	}
+
+	public String getTitle() {
+		return song_name;
+	}
+
+	public String getArtist() {
+		return song_artist;
+	}
+
+	public String getAlbum() {
+		return song_album;
+	}
+
+	public void setTitle(String s) {
+		song_name = s;
+	}
+
+	public void setArtist(String s) {
+		song_artist = s;
+	}
+
+	public void setAlbum(String s) {
+		song_album = s;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+}
---
+++ src/main/java/swe/PotatoStreamer/Loader.java
@@ -0,0 +1,58 @@
+package swe.PotatoStreamer;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.farng.mp3.MP3File;
+import org.farng.mp3.TagException;
+
+@SuppressWarnings("unused")
+public class Loader {
+	/**
+	 * shell class to manage upload/download to/from server
+	 */
+
+	public Loader() throws SQLException {
+		DBInteract.makeConn();
+	}
+
+	/**
+	 * 
+	 * method to send a basic input audio file to the db
+	 * 
+	 * @param file
+	 * @return boolean indicating whether the upload was successful or not
+	 */
+	public boolean uploadSong(AudioFile file, AudioMedia song) throws SQLException {
+		try {
+			DBInteract.addNewMusic(file.file.getMp3file(), song.getUsername(), song.getTitle(), song.getArtist(),
+					song.getAlbum());
+			return true;
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+
+	}
+
+	public AudioFile downloadSong(AudioMedia song) throws IOException, TagException {
+		/**
+		 * song only needs to contain a username provided by the input
+		 */
+		AudioFile aF = null;
+		try {
+			File test = DBInteract.getMusicFromUser(song);
+			MP3File file = new MP3File();
+			file.setMp3file(test);
+			aF = new AudioFile(file);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return aF;
+
+	}
+}
